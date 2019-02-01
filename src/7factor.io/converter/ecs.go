de9af495c0c8188614cfa47cf8d99283c6210a0f
jduv <jduv@7factor.io>
2019-02-01 13:56:55 -0500

    Passing test for transforming single line file

--- src/7factor.io/converter/ecs.go
+++ src/7factor.io/converter/ecs.go
@@ -1,11 +1,39 @@
 package converter
 
-import "errors"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
+type Pair struct {
+	Name string `json:"name"`
+	Value string `json:"value"`
+}
 
 func Transform(contents string) (string, error) {
 	if contents == "" {
 		return `[]`, errors.New("contents cannot be empty")
 	}
 
-	return contents, nil
+	var pairs []Pair
+	split := strings.Split(contents,"=")
+	pairs = append(pairs, Pair{
+		Name: split[0],
+		Value: split[1],
+	})
+
+	return Translate(pairs)
+}
+
+func Translate(pairs []Pair) (string, error) {
+	buffer := bytes.Buffer{}
+	err := json.NewEncoder(&buffer).Encode(pairs)
+
+	if err != nil {
+		return "", err
+	} else {
+		return strings.TrimSpace(buffer.String()), nil
+	}
 }
