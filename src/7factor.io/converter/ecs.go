c76039c8d98d5448d5eef6aaaae3ea6eb25b61a4
chris-malloy <chrismalloymusic@gmail.com>
2019-02-01 11:45:18 -0500

    Transform func nil case passes.

--- src/7factor.io/converter/ecs.go
+++ src/7factor.io/converter/ecs.go
@@ -1,2 +1,11 @@
 package converter
 
+import "errors"
+
+func Transform(contents string) (string, error) {
+	if contents == "" {
+		return `[]`, errors.New("contents cannot be empty")
+	}
+
+	return contents, nil
+}
