f4a63e42a5ac87b9b5bb3bfd3abeed3e8d25f3be
chris-malloy <chrismalloymusic@gmail.com>
2019-02-04 15:45:12 -0500

    Writing and passing cli args nil case.

---
+++ src/7factor.io/args/args.go
@@ -0,0 +1,23 @@
+package args
+
+import (
+	"errors"
+	"github.com/pborman/getopt/v2"
+)
+
+// Default return should at a minimum have the
+// InFile value populated with something.
+type Config struct {
+	EnvironmentFile string
+}
+
+func GetArguments() (Config, error) {
+	getopt.Parse()
+	args := getopt.Args()
+
+	if len(args) <= 1 {
+		return Config{}, errors.New("did not find file to parse")
+	}
+
+	return Config{}, nil
+}
