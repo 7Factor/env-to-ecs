0f33b4d1765728e3c06f3fb9c7663b14977fbfce
chris-malloy <chrismalloymusic@gmail.com>
2019-02-05 04:42:30 -0500

    Output flag nil case is passing.

--- src/7factor.io/_unittests/args_test.go
+++ src/7factor.io/_unittests/args_test.go
@@ -9,9 +9,11 @@ import (
 
 var mockNoArgs = []string{}
 var mockInfileArg = []string{"valid_path.env"}
+var mockWithOutputShortFlag = []string{"valid_path.env", "-o"}
+var mockWithOutputLongFlag = []string{"valid_path.env", "--output"}
 
 var _ = Describe("The argument parser", func() {
-	Context("When not passed `FILE` arg", func() {
+	Context("When not passed `INFILE` arg", func() {
 		It("Returns an error.", func() {
 			os.Args = mockNoArgs
 			_, err := args.GetArguments()
@@ -24,7 +26,19 @@ var _ = Describe("The argument parser", func() {
 			os.Args = mockInfileArg
 			config, err := args.GetArguments()
 			Expect(err).To(BeNil())
-			Expect(config.EnvironmentFile).To(Equal("valid_path.env"))
+			Expect(config.InFile).To(Equal("valid_path.env"))
+		})
+	})
+
+	Context("The `-o, --output` flag", func() {
+		It("Errors with empty argument.", func() {
+			os.Args = mockWithOutputShortFlag
+			_, err := args.GetArguments()
+			Expect(err).ToNot(BeNil())
+
+			os.Args = mockWithOutputLongFlag
+			_, err = args.GetArguments()
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
