5cebd4823a79103a665efa7c70d17a10d7941611
chris-malloy <chrismalloymusic@gmail.com>
2019-02-06 16:08:45 -0500

    Overhauling integration tests, they now build and test the binary end to end.

--- src/7factor.io/cmd/main.go
+++ src/7factor.io/cmd/main.go
@@ -11,8 +11,14 @@ func main() {
 	config, err := args.GetArguments()
 
 	if err != nil {
-		fmt.Fprint(os.Stderr, err)
+		fmt.Fprintf(os.Stderr, "received error while getting args: %v\n", err)
+		os.Exit(1)
 	}
 
-	converter.ReadAndConvert(config.InFile, config.OutFile)
+	_, err = converter.ReadAndConvert(config.InFile, config.OutFile)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "recieved error while reading and converting args: %v\n", err)
+		os.Exit(1)
+	}
 }
