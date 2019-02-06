6802689b43d2beb1fd65d732f4291088f61876b3
chris-malloy <chrismalloymusic@gmail.com>
2019-02-06 16:08:45 -0500

    Overhauling integration tests, they now build and test the binary end to end.

---
+++ src/7factor.io/_inttests/cmd_test.go
@@ -0,0 +1,78 @@
+package _inttests
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+	"os/exec"
+)
+
+var pathToCMD = "7factor.io/cmd"
+
+var _ = Describe("Compiling and running the script with arguments", func() {
+	BeforeSuite(func() {
+		var err error
+		pathToCMD, err = Build("7factor.io/cmd")
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+
+	AfterSuite(func() {
+		CleanupBuildArtifacts()
+	})
+
+	Context("When script is called with no INFILE", func() {
+		It("Errors and exits in the expected manner.", func() {
+			command := exec.Command(pathToCMD)
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			session.Wait()
+			Eventually(session).Should(Exit(1))
+			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+		})
+	})
+
+	Context("When the script is called with an INFILE that does no exist", func() {
+		It("Errors and exits in the expected manner.", func() {
+			command := exec.Command(pathToCMD, "/path/does/not/exist/ever")
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			session.Wait()
+			Eventually(session).Should(Exit(1))
+			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+		})
+	})
+
+	Context("When calling the script with INFILE only", func() {
+		It("Prints the output to stdout", func() {
+			command := exec.Command(pathToCMD, "valid_path.env")
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			session.Wait()
+			Expect(session).Should(Exit(0))
+			Eventually(session.Out).Should(gbytes.Say(`[{"name":"FOO","value":"bar"},{"name":"BAZ","value":"boo"}]`))
+		})
+	})
+
+	Context("When calling the script with -o but no specified outfile", func() {
+		It("Errors and exits in the expected manner.", func() {
+			command := exec.Command(pathToCMD, "valid_path.env", "-o")
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			session.Wait()
+			Eventually(session).Should(Exit(1))
+			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+		})
+	})
+
+	Context("When calling the script with -o and passing a valid output file", func() {
+		It("Writes to the outfile with no errors and exits cleanly.", func() {
+			command := exec.Command(pathToCMD, "valid_path.env", "-o", "output.json")
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).ShouldNot(HaveOccurred())
+			session.Wait()
+			Eventually(session).Should(Exit(0))
+			Eventually(session.Err.Contents()).Should(BeEmpty())
+		})
+	})
+})
--- src/7factor.io/converter/common.go
+++ src/7factor.io/converter/common.go
@@ -10,23 +10,29 @@ func ReadAndConvert(inFile string, outFile string) (string, error) {
 	// verify inFile exists
 	_, err := os.Stat(inFile)
 	if err != nil {
-		return "", fmt.Errorf("inFile not found")
+		return "", fmt.Errorf("INFILE not found")
 	}
 
 	// parse inFile
 	contents, err := ioutil.ReadFile(inFile)
 	if err != nil {
-		return "", fmt.Errorf("unable to read inFile, catestrophic error")
+		return "", fmt.Errorf("unable to read INFILE, catestrophic error")
 	}
 
-	// write to outFile
-	// WriteFile will create the file if it does not exist
+	// transform contents
 	transformedContents, err := TransformAndTranslate(string(contents))
-	if err != nil {
-		return "", fmt.Errorf("error while transforming contents")
+
+	// write to outFile
+	if outFile == "stdout" {
+		fmt.Fprint(os.Stdout, transformedContents)
+	} else {
+		// WriteFile will create the file if it does not exist
+		if err != nil {
+			return "", fmt.Errorf("error while transforming contents")
+		}
+		n1 := []byte(transformedContents)
+		err = ioutil.WriteFile(outFile, n1, 0644)
 	}
-	n1 := []byte(transformedContents)
-	err = ioutil.WriteFile(outFile, n1, 0644)
 
 	return outFile, nil
 }
