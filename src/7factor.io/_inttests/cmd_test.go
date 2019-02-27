ac2a6bd02e9918f428a9057f1dc5ce8d178b2d50
chris-malloy <chrismalloymusic@gmail.com>
2019-02-27 11:32:33 -0500

    Reorganizing integration test into logical describe blocks.

--- src/7factor.io/_inttests/cmd_test.go
+++ src/7factor.io/_inttests/cmd_test.go
@@ -21,109 +21,117 @@ var _ = Describe("Compiling and running the script with arguments", func() {
 		Expect(err).ShouldNot(HaveOccurred())
 	})
 
-	Context("When script is called with no INFILE", func() {
-		It("Errors and exits in the expected manner.", func() {
-			command := exec.Command(pathToCMD)
-			session := setUpSessionAndWait(command)
+    Describe("The -i | --infile flag", func() {
+		Context("When script is called with no INFILE", func() {
+			It("Errors and exits in the expected manner.", func() {
+				command := exec.Command(pathToCMD)
+				session := setUpSessionAndWait(command)
 
-			Eventually(session).Should(Exit(withLinuxErrorCode))
-			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+				Eventually(session).Should(Exit(withLinuxErrorCode))
+				Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+			})
+		})
+
+		Context("When the script is called with an INFILE that does no exist", func() {
+			It("Errors and exits in the expected manner.", func() {
+				command := exec.Command(pathToCMD, "-i", "/path/does/not/exist/ever")
+				session, err := Start(command, GinkgoWriter, GinkgoWriter)
+				Expect(err).ShouldNot(HaveOccurred())
+				session.Wait()
+
+				Eventually(session).Should(Exit(withLinuxErrorCode))
+				Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+			})
+		})
+
+		Context("When calling the script with INFILE only", func() {
+			It("Prints the output to stdout", func() {
+				command := exec.Command(pathToCMD, "-i", "valid_path.env")
+				session := setUpSessionAndWait(command)
+
+				Expect(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
+
+				Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutput))
+			})
 		})
 	})
 
-	Context("When the script is called with an INFILE that does no exist", func() {
-		It("Errors and exits in the expected manner.", func() {
-			command := exec.Command(pathToCMD, "-i", "/path/does/not/exist/ever")
-			session, err := Start(command, GinkgoWriter, GinkgoWriter)
-			Expect(err).ShouldNot(HaveOccurred())
-			session.Wait()
+	Describe("The -o | --outfile flag", func() {
+		Context("When calling the script with --output but no specified outfile", func() {
+			It("Errors and exits in the expected manner.", func() {
+				command := exec.Command(pathToCMD, "-i", "valid_path.env", "-o")
+				session := setUpSessionAndWait(command)
 
-			Eventually(session).Should(Exit(withLinuxErrorCode))
-			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+				Eventually(session).Should(Exit(withLinuxErrorCode))
+				Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+			})
+		})
+
+		Context("When calling the script with --output and passing a valid output file", func() {
+			It("Writes to the outfile with no errors and exits cleanly.", func() {
+				command := exec.Command(pathToCMD, "-i", "valid_path.env", "-o", "output.json")
+				session := setUpSessionAndWait(command)
+
+				Eventually(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
+
+				actual, err := ioutil.ReadFile("output.json")
+				Expect(err).To(BeNil())
+				Expect(string(actual)).To(Equal(expectedOutput))
+			})
 		})
 	})
 
-	Context("When calling the script with INFILE only", func() {
-		It("Prints the output to stdout", func() {
-			command := exec.Command(pathToCMD, "-i", "valid_path.env")
-			session := setUpSessionAndWait(command)
+	Describe("The -v | --variable flag", func() {
+		Context("When calling the script with -v and passing a valid variable", func() {
+			It("Writes the correct output with no errors and exits cleanly.", func() {
+				command := exec.Command(pathToCMD, "-i", "valid_path.env", "-v", "extra_var=a_database_connection_string")
+				session := setUpSessionAndWait(command)
 
-			Expect(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
+				Eventually(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
 
-			Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutput))
+				Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithExtraVar))
+			})
+		})
+
+		Context("When calling the script with multiple -v flags and passing valid variables", func() {
+			It("Writes the correct output with no errors and exits cleanly.", func() {
+				command := exec.Command(pathToCMD, "-i", "valid_path.env", "-v", "extra_var1=this_thing", "-v", "extra_var2=that_thing")
+				session := setUpSessionAndWait(command)
+
+				Eventually(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
+
+				Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithMultipleExtraVar))
+			})
 		})
 	})
 
-	Context("When calling the script with --output but no specified outfile", func() {
-		It("Errors and exits in the expected manner.", func() {
-			command := exec.Command(pathToCMD, "-i", "valid_path.env", "-o")
-			session := setUpSessionAndWait(command)
+	Describe("Handling files with no newlines", func() {
+		Context("When the script is called with an INFILE that has no newline at the end", func() {
+			It("Writes to the outfile correctly with no errors", func() {
+				command := exec.Command(pathToCMD, "-i", "with_no_new_line.env")
+				session := setUpSessionAndWait(command)
 
-			Eventually(session).Should(Exit(withLinuxErrorCode))
-			Eventually(session.Err.Contents()).ShouldNot(BeEmpty())
+				Expect(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
+
+				Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutput))
+			})
 		})
-	})
 
-	Context("When calling the script with --output and passing a valid output file", func() {
-		It("Writes to the outfile with no errors and exits cleanly.", func() {
-			command := exec.Command(pathToCMD, "-i", "valid_path.env", "-o", "output.json")
-			session := setUpSessionAndWait(command)
+		Context("When the script is called with an INFILE that has no newline at the end and passing extra variables", func() {
+			It("Writes to the outfile correctly with no errors", func() {
+				command := exec.Command(pathToCMD, "-i", "with_no_new_line.env", "-v", "extra_var=a_database_connection_string")
+				session := setUpSessionAndWait(command)
 
-			Eventually(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
+				Expect(session).Should(Exit(withLinuxPassingCode))
+				Eventually(session.Err.Contents()).Should(BeEmpty())
 
-			actual, err := ioutil.ReadFile("output.json")
-			Expect(err).To(BeNil())
-			Expect(string(actual)).To(Equal(expectedOutput))
-		})
-	})
-
-	Context("When calling the script with -v and passing a valid variable", func() {
-		It("Writes the correct output with no errors and exits cleanly.", func() {
-			command := exec.Command(pathToCMD, "-i", "valid_path.env", "-v", "extra_var=a_database_connection_string")
-			session := setUpSessionAndWait(command)
-
-			Eventually(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
-
-			Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithExtraVar))
-		})
-	})
-
-	Context("When calling the script with multiple -v flags and passing valid variables", func() {
-		It("Writes the correct output with no errors and exits cleanly.", func() {
-			command := exec.Command(pathToCMD, "-i", "valid_path.env", "-v", "extra_var1=this_thing", "-v", "extra_var2=that_thing")
-			session := setUpSessionAndWait(command)
-
-			Eventually(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
-
-			Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithMultipleExtraVar))
-		})
-	})
-
-	Context("When the script is called with an INFILE that has no newline at the end", func() {
-		It("Writes to the outfile correctly with no errors", func() {
-			command := exec.Command(pathToCMD, "-i", "with_no_new_line.env")
-			session := setUpSessionAndWait(command)
-
-			Expect(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
-
-			Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutput))
-		})
-	})
-
-	Context("When the script is called with an INFILE that has no newline at the end and passing extra variables", func() {
-		It("Writes to the outfile correctly with no errors", func() {
-			command := exec.Command(pathToCMD, "-i", "with_no_new_line.env", "-v", "extra_var=a_database_connection_string")
-			session := setUpSessionAndWait(command)
-
-			Expect(session).Should(Exit(withLinuxPassingCode))
-			Eventually(session.Err.Contents()).Should(BeEmpty())
-
-			Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithExtraVar))
+				Expect(string(session.Out.Contents())).To(ContainSubstring(expectedOutputWithExtraVar))
+			})
 		})
 	})
 
