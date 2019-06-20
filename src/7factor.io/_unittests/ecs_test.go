0e4d6777e6020be10e61654ebcaa3c55c332a175
ThrDev <admin@thrdu.de>
2019-06-20 16:26:08 -0400

    Rewrite ecs.go module. Fix bug with allowing for quoted fields. Fix bug with allowing for fields with spaces i.e A = B

--- src/7factor.io/_unittests/ecs_test.go
+++ src/7factor.io/_unittests/ecs_test.go
@@ -20,6 +20,7 @@ L=M
 N=O
 `
 const SingleLineMultiInput = `W=X   Y=Z`
+const InputWithSpaces = `X = 1 Y = 1`
 const illegalJSONInput = `A=\"`
 const MultiLineWithComments = `
 # this is a comment
@@ -36,6 +37,11 @@ B=C
 const InputWithHash = `WITHHASH=#FOO#`
 
 const InputWithQuotes = `WITHQUOTES="this is a test"`
+const InputWithQuotesAndSpaces = `A = "test string"`
+
+const CrazyInput = `A=1 B = 2 C="test string" D = "another test string" E="1" F = "2"
+G = "another test string"
+H="test string"`
 
 var _ = Describe("The ECS converter", func() {
 	Context("When passed a blank file", func() {
@@ -51,6 +57,22 @@ var _ = Describe("The ECS converter", func() {
 			converted, err := converter.TransformAndTranslate(SingleLineSingleInput)
 			Expect(err).To(BeNil())
 			Expect(converted).To(Equal(`[{"name":"A","value":"B"}]`))
+		})
+	})
+
+	Context("When passed a single line file with multiple values and spaces between the equals", func() {
+		It("Returns the expected JSON blob", func() {
+			converted, err := converter.TransformAndTranslate(InputWithSpaces)
+			Expect(err).To(BeNil())
+			Expect(converted).To(Equal(`[{"name":"X","value":"1"},{"name":"Y","value":"1"}]`))
+		})
+	})
+
+	Context("When passed a single line file with multiple values and spaces between the equals", func() {
+		It("Returns the expected JSON blob", func() {
+			converted, err := converter.TransformAndTranslate(InputWithQuotesAndSpaces)
+			Expect(err).To(BeNil())
+			Expect(converted).To(Equal(`[{"name":"A","value":"test string"}]`))
 		})
 	})
 
@@ -118,11 +140,19 @@ var _ = Describe("The ECS converter", func() {
 		})
 	})
 
-		Context("When called with a quoted string variable", func() {
+	Context("When called with a quoted string variable", func() {
 		It("Works as expected and doesn't throw an index out of range exception.", func() {
 			converted, err := converter.TransformAndTranslate(InputWithQuotes)
 			Expect(err).To(BeNil())
 			Expect(converted).To(Equal(`[{"name":"WITHQUOTES","value":"this is a test"}]`))
+		})
+	})
+
+	Context("When called with a string with multiple challenges", func() {
+		It("Works as expected and doesn't throw an index out of range exception.", func() {
+			converted, err := converter.TransformAndTranslate(CrazyInput)
+			Expect(err).To(BeNil())
+			Expect(converted).To(Equal(`[{"name":"A","value":"1"},{"name":"B","value":"2"},{"name":"C","value":"test string"},{"name":"D","value":"another test string"},{"name":"E","value":"1"},{"name":"F","value":"2"},{"name":"G","value":"another test string"},{"name":"H","value":"test string"}]`))
 		})
 	})
 })
--- src/7factor.io/converter/ecs.go
+++ src/7factor.io/converter/ecs.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -14,14 +13,116 @@ type Pair struct {
 	Value string `json:"value"`
 }
 
+
 func TransformAndTranslate(contents string) (string, error) {
 	if contents == "" {
 		return `[]`, errors.New("contents cannot be empty")
 	}
 
-	cleanedContents := cleanContents(contents)
-	pairs := transform(cleanedContents)
+	var itemsToBeParsed []string
+	for _, line := range strings.Split(contents, "\n") {
+		if hasComment(line) || hasEmptyString(line) {
+			continue
+		}
+		items := parse(line)
+		
+
+		itemsToBeParsed = append(itemsToBeParsed, items...)
+	}
+
+	pairs := transform(itemsToBeParsed)
 	return translate(pairs)
+}
+
+var commentRegex = regexp.MustCompile("(?m)^\\s*\\#.*$")
+func hasComment(line string) bool {
+	return commentRegex.MatchString(line)
+}
+
+func hasEmptyString(line string) bool {
+	return len(line) == 0 || line == ""
+}
+
+var singleValueContextRegex = regexp.MustCompile("^.+?=.+?$")
+func parse(line string) []string {
+	defer func() {
+		// Clear out newItems for next call
+		newItems = nil
+	}()
+	// There could be multiple values on a single line, so take that into consideration.
+	items := strings.Fields(line)
+	for i := 0; i < len(items); i++ {
+		item := items[i]
+		
+		// Try to handle items with regular quotes i.e A="B"
+		if handleQuotes(item) {
+			continue
+		}
+		
+		// Try to handle items with "Key=Value"
+		if singleValueContextRegex.MatchString(item) {
+			newItems = append(newItems, item)
+			continue
+		}
+
+		// Try to predict the next spaced key/val pair to see if it has quotes.
+		if handleSpacedQuotes(item, items, i) {
+			i++
+			continue
+		}
+
+		// Try and pull next 2 items and append together to create a statement (i.e X=1).
+		newItems = append(newItems, item+items[i+1]+items[i+2])
+		
+		// Skip the next two items, because we've already inserted them into our item list.
+		i+=2
+	}
+	
+	return newItems
+}
+
+var newItems []string
+var tempSpacedItem string
+var quotedItems []string
+
+var parsingQuotes = false
+var parsingSpacedQuotes = false
+func handleQuotes(item string) bool {
+	if strings.Contains(item, "\"") && !strings.HasSuffix(item, "\"") {
+		parsingQuotes = true
+	}
+
+	if parsingQuotes {
+		quotedItems = append(quotedItems, item)
+		if strings.HasSuffix(item, "\"") {
+			if parsingSpacedQuotes {
+				tempSpacedItem += strings.Join(quotedItems, " ")
+				newItems = append(newItems, tempSpacedItem)
+			} else {
+				newItems = append(newItems, strings.Join(quotedItems, " "))
+			}
+			resetItems()
+		}
+		return true
+	}
+	return  false
+}
+
+func handleSpacedQuotes(item string, items []string, iterator int) bool {
+	if strings.Contains(items[iterator+2], "\"") && !strings.HasSuffix(items[iterator+2], "\"") {
+		parsingQuotes = true
+		parsingSpacedQuotes = true
+		tempSpacedItem = item + items[iterator+1]
+		return true
+	}
+	return false
+}
+
+func resetItems() {
+	parsingSpacedQuotes = false
+	parsingQuotes = false
+	tempSpacedItem = ""
+	quotedItems = nil
 }
 
 func transform(slice []string) []Pair {
@@ -32,7 +133,7 @@ func transform(slice []string) []Pair {
 		splitOnEquals = strings.SplitN(item, "=", 2)
 		pairs = append(pairs, Pair{
 			Name:  splitOnEquals[0],
-			Value: splitOnEquals[1],
+			Value: trimQuotes(splitOnEquals[1]),
 		})
 	}
 
@@ -50,44 +151,11 @@ func translate(pairs []Pair) (string, error) {
 	}
 }
 
-func cleanContents(dirtyString string) []string {
-	withoutComments := removeComments(dirtyString)
-	newLines := splitOnNewLine(withoutComments)
-	slice := splitOnWhiteSpace(newLines)
-	cleanedSlice := removeEmptyStrings(slice)
-	return cleanedSlice
-}
-
-func removeComments(stringWithComments string) string {
-	re := regexp.MustCompile("(?m)^\\s*\\#.*$")
-	withoutComments := re.ReplaceAllString(stringWithComments, "")
-	return withoutComments
-}
-
-func splitOnNewLine(stringWithNewLines string) []string {
-	str := strings.Split(stringWithNewLines, "\n")
-	return str
-}
-
-func splitOnWhiteSpace(stringWithWhiteSpace []string) []string {
-	var itemList []string
-	for _, str := range stringWithWhiteSpace {
-		noWhiteSpace := strings.Fields(str)
-		newStr := strings.Join(noWhiteSpace, "")
-		itemList = append(itemList, newStr)
-	}
-
-	fmt.Println(itemList)
-	return itemList
-}
-
-func removeEmptyStrings(sliceWithEmptyStrings []string) []string {
-	var noEmptyStrings []string
-	for _, str := range sliceWithEmptyStrings {
-		if str != "" {
-			noEmptyStrings = append(noEmptyStrings, str)
+func trimQuotes(s string) string {
+	if len(s) >= 2 {
+		if s[0] == '"' && s[len(s)-1] == '"' {
+			return s[1 : len(s)-1]
 		}
 	}
-
-	return noEmptyStrings
+	return s
 }
