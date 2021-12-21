49e294705cc83e4425d123f4c08b630615204fec
Nate Fulton <nate.fulton@7factor.io>
2021-12-21 12:58:53 -0500

    Allow vars in quotes to contain newlines

--- src/7factor.io/converter/ecs.go
+++ src/7factor.io/converter/ecs.go
@@ -13,25 +13,28 @@ type Pair struct {
 	Value string `json:"value"`
 }
 
-
-func ConvertInputToJson(contents string) (string, error) {
-	if contents == "" {
+func ConvertInputToJson(contents []string) (string, error) {
+	if len(contents) == 0 {
 		return `[]`, errors.New("contents cannot be empty")
 	}
-	itemsToBeParsed := handleInputString(contents)
+	itemsToBeParsed := handleInputSlice(contents)
 
 	pairs := splitOnEquals(itemsToBeParsed)
 	return pairsToString(pairs)
 }
 
-func handleInputString(contents string) []string {
+var assignmentRegex = regexp.MustCompile("\\w+ *= *(?:'[^']*'|\"[^\"]*\"|[^\\s=]*)=?")
+
+func handleInputSlice(contents []string) []string {
 	var itemsToBeParsed []string
-	for _, line := range strings.Split(contents, "\n") {
-		if hasComment(line) || hasEmptyString(line) {
+	for _, line := range contents {
+		line = stripComments(line)
+
+		if hasEmptyString(line) {
 			continue
 		}
 
-		items := processSingleLine(line)
+		items := assignmentRegex.FindAllString(line, -1)
 
 		itemsToBeParsed = append(itemsToBeParsed, items...)
 	}
@@ -39,109 +42,14 @@ func handleInputString(contents string) []string {
 	return itemsToBeParsed
 }
 
-var commentRegex = regexp.MustCompile("(?m)^\\s*\\#.*$")
-func hasComment(line string) bool {
-	return commentRegex.MatchString(line)
+var commentRegex = regexp.MustCompile("^(?m)\\s*#.*\\n*$")
+
+func stripComments(line string) string {
+	return commentRegex.ReplaceAllLiteralString(line, "")
 }
 
 func hasEmptyString(line string) bool {
 	return len(line) == 0 || line == ""
-}
-
-var singleValueContextRegex = regexp.MustCompile("^.+?=.+?$")
-func processSingleLine(line string) []string {
-	defer func() {
-		// Clear out newItems for next call
-		newItems = nil
-	}()
-	// There could be multiple values on a single line, so take that into consideration.
-	items := strings.Fields(line)
-	for i := 0; i < len(items); i++ {
-		item := items[i]
-		
-		// Try to handle items with regular quotes i.e A="B"
-		if handleQuotes(item) {
-			continue
-		}
-		
-		// Try to handle items with "Key=Value"
-		if singleValueContextRegex.MatchString(item) {
-			newItems = append(newItems, item)
-			continue
-		}
-
-		if handleEmptyVal(item, items, i) {
-			newItems = append(newItems, item)
-			continue
-		}
-
-		// Try to predict the next spaced key/val pair to see if it has quotes.
-		if handleSpacedQuotes(item, items, i) {
-			i++
-			continue
-		}
-
-		// Try and pull next 2 items and append together to create a statement (i.e X=1).
-		newItems = append(newItems, item+items[i+1]+items[i+2])
-		// Skip the next two items, because we've already inserted them into our item list.
-		i+=2
-	}
-	
-	return newItems
-}
-
-var newItems []string
-var tempSpacedItem string
-var quotedItems []string
-
-var parsingQuotes = false
-var parsingSpacedQuotes = false
-func handleQuotes(item string) bool {
-	isQuotes := false
-	if strings.Contains(item, "\"") && !strings.HasSuffix(item, "\"") {
-		parsingQuotes = true
-	}
-
-	if parsingQuotes {
-		quotedItems = append(quotedItems, item)
-		if strings.HasSuffix(item, "\"") {
-			if parsingSpacedQuotes {
-				tempSpacedItem += strings.Join(quotedItems, " ")
-				newItems = append(newItems, tempSpacedItem)
-			} else {
-				newItems = append(newItems, strings.Join(quotedItems, " "))
-			}
-			resetItems()
-		}
-		isQuotes = true
-	}
-	return  isQuotes
-}
-
-func handleEmptyVal(item string, items []string, i int) bool {
-	if len(items) < 2 {
-		tempSpacedItem = item
-		return true
-	}
-	return false
-}
-
-func handleSpacedQuotes(item string, items []string, iterator int) bool {
-	isSpacedQuotes := false
-	if strings.Contains(items[iterator+2], "\"") && !strings.HasSuffix(items[iterator+2], "\"") {
-		parsingQuotes = true
-		parsingSpacedQuotes = true
-		tempSpacedItem = item + items[iterator+1]
-		isSpacedQuotes = true
-	}
-	return isSpacedQuotes
-}
-
-func resetItems() {
-	parsingSpacedQuotes = false
-	parsingQuotes = false
-	tempSpacedItem = ""
-	quotedItems = nil
 }
 
 func splitOnEquals(slice []string) []Pair {
@@ -151,8 +59,8 @@ func splitOnEquals(slice []string) []Pair {
 	for _, item := range slice {
 		splitOnEquals = strings.SplitN(item, "=", 2)
 		pairs = append(pairs, Pair{
-			Name:  splitOnEquals[0],
-			Value: trimQuotes(splitOnEquals[1]),
+			Name:  strings.TrimSpace(splitOnEquals[0]),
+			Value: trimQuotes(strings.TrimSpace(splitOnEquals[1])),
 		})
 	}
 
